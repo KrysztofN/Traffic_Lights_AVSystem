8cb339f78aea0ae347f492a6188238c10e029a15
Kris <kris1toja@gmail.com>
2026-02-12 10:59:33 +0200

    [TOOL] initial spring boot app push

---
+++ backend/traffic_light_rest/src/main/java/com/kris/traffic_light_rest/TrafficLightRestApplication.java
@@ -0,0 +1,13 @@
+package com.kris.traffic_light_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficLightRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrafficLightRestApplication.class, args);
+    }
+
+}
